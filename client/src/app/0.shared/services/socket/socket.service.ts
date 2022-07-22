4a9a5785bc09fff55f54b830221842c150a776af
ksmnsmarts <ksm@nsmarts.co.kr>
2022-07-22 09:50:06 +0900

    socket

--- client/src/app/0.shared/services/socket/socket.service.ts
+++ client/src/app/0.shared/services/socket/socket.service.ts
@@ -5,8 +5,7 @@ import { environment } from '../../../../environments/environment';
     providedIn: 'root'
 })
 export class SocketService {
-    // private url = 'http://localhost:8081'; // comclass 통합전 서버주소
-    // private url = 'http://localhost:3000/socketWebRTC';
+
     private url = environment.socketUrl;
     private _socket: Socket;
 
@@ -15,7 +14,6 @@ export class SocketService {
         // this._socket.open으로 수동으로 연결
         // 이렇게 하는 이유 : 뒤로 가기로 소켓연결을 끊었을 경우(socket.close()), 다시 컴포넌트 접속시 자동 연결이 안돼서 수동으로 바꿈
         this._socket = io(this.url + '/socket', { transports: ['websocket'], path: '/socket', autoConnect: false });
-        // console.log(this._socket);
     }
 
     get socket() {
